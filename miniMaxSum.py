a4df5b12ec62e3f49575710339537e37f9b09a7a
RomainMichau <31858651+RomainMichau@users.noreply.github.com>
2025-09-12 18:46:04 +0200

    Update miniMaxSum.py

--- miniMaxSum.py
+++ miniMaxSum.py
@@ -12,29 +12,32 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def bubble_sorting2(lst) -> list :
-    n = len(lst)
-    changed = True
+# Utilise la fonction `.sort` de python
+# Elle sera toujours plus perfommante que la tienne
+# Par example bubble_sorting c'est O(n * n) alors que il y a des algos en O(log n * n) 
 
-    while changed:
-        changed = False
-        for i in range(n - 1):
-            if lst[i] > lst[i+1]:
-                lst[i], lst[i+1] = lst[i+1], lst[i]
-                changed = True
-        n -= 1
-    return lst
 
-def miniMaxSum(arr) -> None:
-    
-    arr = bubble_sorting2(lst=arr)
-    print(f"{sum(arr[:4])} {sum(arr[-4:])}")
+# def bubble_sorting2(lst) -> list :
+#     n = len(lst)
+#     changed = True
+
+#     while changed:
+#         changed = False
+#         for i in range(n - 1):
+#             if lst[i] > lst[i+1]:
+#                 lst[i], lst[i+1] = lst[i+1], lst[i]
+#                 changed = True
+#         n -= 1
+#     return lst
+
+# C'est mieux si ta fonction retourne une valeur plutot que print
+def miniMaxSum(arr) -> (int, int):
+    arr.sort()
+    return (sum(arr[:4]),sum(arr[-4:]))
 
 
     
 
 if __name__ == '__main__':
-
     arr = list(map(int, input().rstrip().split()))
-
-    miniMaxSum(arr)
+    print(*miniMaxSum(arr))
