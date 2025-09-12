ab6cec46b387895b827439f5b41bfe3271130859
Unrectified <nathamichau@gmail.com>
2025-09-12 16:46:49 +0200

    first commit

---
+++ plus_minus.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    # Write your code here
+    print("hey")
+    print(arr)
+    return([1,2])
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
