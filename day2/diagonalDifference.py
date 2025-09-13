235e0586abef395e4b9ab74ae8c6b8560b1d87f1
Unrectified <nathamichau@gmail.com>
2025-09-13 13:40:35 +0200

    day2

---
+++ day2/diagonalDifference.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-diagonal-difference/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr): #// Complexity: O(n)
+    lenght = len(arr)
+    diag_left = 0
+    diag_right = 0
+
+    for i, lign in enumerate(arr):
+        diag_left += lign[i]           # diagonale gauche
+        diag_right += lign[lenght-i-1]  # diagonale droite
+
+    return abs(diag_left - diag_right)
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
---
+++ day2/lonelyinteger.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+# https://www.hackerrank.com/challenges/one-week-preparation-kit-lonely-integer/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a): #// Complexity: O(n²)
+    # Write your code here
+    for e in  a:
+        occurence = a.count(e)
+        if occurence == 1:
+            return e
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
