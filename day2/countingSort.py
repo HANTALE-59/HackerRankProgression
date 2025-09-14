097243e0fb942216176cbe274bb7a53f49abfae3
Unrectified <nathamichau@gmail.com>
2025-09-14 15:40:47 +0200

    Counting sort

---
+++ day2/countingSort.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-countingsort1/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-two
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr): #//Complexity: O(k+n)
+
+    k = 100 #// number range
+    frequency = [0] * k
+    for e in arr:
+        frequency[e] = frequency[e] + 1
+    return(frequency)
+
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
